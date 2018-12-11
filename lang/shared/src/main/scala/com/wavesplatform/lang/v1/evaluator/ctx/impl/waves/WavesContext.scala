6e22399b7791535fe7c9978aac4c56490c7ad76c
Ilya Smagin <ilya.smagin@gmail.com>
2018-12-11 16:49:16 +0300

    NODE-1299: compiler test for verifier function

--- lang/shared/src/main/scala/com/wavesplatform/lang/v1/evaluator/ctx/impl/waves/WavesContext.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v1/evaluator/ctx/impl/waves/WavesContext.scala
@@ -363,4 +363,6 @@ object WavesContext {
 
     CTX(Types.wavesTypes ++ (if (version == V3) List(writeSetType) else List.empty), commonVars ++ vars(version), functions)
   }
+
+  val transactionsCommonType = UnionType("Transaction", buildActiveTransactionTypes(true).map(_.typeRef))
 }
