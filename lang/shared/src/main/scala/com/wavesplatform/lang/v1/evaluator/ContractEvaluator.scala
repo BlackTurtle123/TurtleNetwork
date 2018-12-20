5aa52fcc310dcc7c7246b51ca1b2a0a8c9734f07
Alexandr Makoed <me@dsfn.gq>
2018-12-20 13:12:29 +0300

    NODE-1299: Declarations compilation, simple integration test

--- lang/shared/src/main/scala/com/wavesplatform/lang/v1/evaluator/ContractEvaluator.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v1/evaluator/ContractEvaluator.scala
@@ -4,12 +4,14 @@ import com.wavesplatform.lang.contract.Contract
 import com.wavesplatform.lang.contract.Contract.VerifierFunction
 import com.wavesplatform.lang.v1.FunctionHeader
 import com.wavesplatform.lang.v1.compiler.Terms._
-import com.wavesplatform.lang.v1.evaluator.ctx.{EvaluationContext, LoggedEvaluationContext}
 import com.wavesplatform.lang.v1.evaluator.ctx.impl.waves.{Bindings, Types}
+import com.wavesplatform.lang.v1.evaluator.ctx.{EvaluationContext, LoggedEvaluationContext}
 import com.wavesplatform.lang.v1.task.imports.raiseError
 import com.wavesplatform.lang.v1.traits.domain.Recipient.Address
 import com.wavesplatform.lang.v1.traits.domain.{DataItem, Tx}
 import scodec.bits.ByteVector
+
+import scala.collection.mutable.ListBuffer
 
 object ContractEvaluator {
   case class Invokation(name: String, fc: FUNCTION_CALL, invoker: ByteVector)
@@ -18,11 +20,13 @@ object ContractEvaluator {
     c.cfs.find(_.u.name == i.name) match {
       case None => raiseError[LoggedEvaluationContext, ExecutionError, EVALUATED](s"Callable function '${i.name} doesn't exist in the contract")
       case Some(f) =>
-        val expr =
-          BLOCKV2(
-            LET(f.c.pubKeyArgName, CONST_BYTEVECTOR(i.invoker)),
-            BLOCKV2(f.u, i.fc)
-          )
+        val zeroExpr = BLOCKV2(
+          LET(f.c.pubKeyArgName, CONST_BYTEVECTOR(i.invoker)),
+          BLOCKV2(f.u, i.fc)
+        )
+        val expr = c.dec.foldRight(zeroExpr) { (d, e) =>
+          BLOCKV2(d, e)
+        }
         EvaluatorV1.evalExpr(expr)
     }
   }
@@ -35,7 +39,10 @@ object ContractEvaluator {
   }
 
   def apply(ctx: EvaluationContext, c: Contract, i: Invokation): Either[ExecutionError, ContractResult] = {
-    val lec = LoggedEvaluationContext(_ => _ => (), ctx)
+    val log = ListBuffer[LogItem]()
+    val llc = (str: String) => (v: LetExecResult) => log.append((str, v))
+    val lec = LoggedEvaluationContext(llc, ctx)
+    println(log mkString "\n")
     eval(c, i).run(lec).value._2.flatMap(ContractResult.fromObj)
 
   }
@@ -104,7 +111,22 @@ object ContractResult {
           case "ContractResult" => processContractSet(c)
           case _                => ???
         }
-      case _ => ???
+        Right(ContractResult(r.toList, List.empty))
+      case CaseObj(tpe, fields) if tpe.name == "TransferSet" =>
+        val xs: IndexedSeq[EVALUATED] = fields("transfers").asInstanceOf[ARR].xs
+        val r = xs.map {
+          case CaseObj(tpe, fields) if tpe.name == "Transfer" =>
+            (fields("recipient"), fields("amount")) match {
+              case (CaseObj(Types.addressType.typeRef, fields2), CONST_LONG(b)) =>
+                fields2("bytes") match {
+                  case CONST_BYTEVECTOR(addBytes) => (Address(addBytes), b)
+                  case v                          => ???
+                }
+              case v => ???
+            }
+          case v => ???
+        }
+        Right(ContractResult(List.empty, r.toList))
     }
   }
 }
