c3646062004bf0af5efe2c4c1bc0f8094019286a
bnagtega <bram.nagtegals@trendminer.com>
2018-12-23 14:47:15 +0100

    Hotfix part 1

--- src/main/scala/com/wavesplatform/state/patch/CancelInvalidTx.scala
+++ src/main/scala/com/wavesplatform/state/patch/CancelInvalidTx.scala
@@ -1,34 +1,28 @@
 package com.wavesplatform.state.patch
 
-import com.wavesplatform.account.Address
-import com.wavesplatform.state.{Blockchain, Diff, EitherExt2, LeaseBalance, Portfolio}
-import com.wavesplatform.utils.ScorexLogging
+import com.wavesplatform.state.{Blockchain, Diff, LeaseBalance, Portfolio}
+import scorex.utils.ScorexLogging
 
-object CancelInvalidTx extends ScorexLogging {
+object CancelAllLeases extends ScorexLogging {
 
   def apply(s: Blockchain): Diff = {
-    val addr1 = Address.fromString("3JcQHUDAzkprtFHX8DskQCys3upBYhPeJQq").explicitGet()
-    val addr2 = Address.fromString("3JbHxyVNbEEJXMDuuR9kPeTmXn5BCDBSQp4").explicitGet()
-    val addr3 = Address.fromString("3JqAYiRnuiJxdMVmdTUsxuTV39LXHR5JWXk").explicitGet()
+    val addr1 = Address.fromString("3JcQHUDAzkprtFHX8DskQCys3upBYhPeJQq")
+    val addr2 = Address.fromString("3JbHxyVNbEEJXMDuuR9kPeTmXn5BCDBSQp4")
+    val addr3 = Address.fromString("3JqAYiRnuiJxdMVmdTUsxuTV39LXHR5JWXk")
+    val bal1 = s.balance(addr1)
+    val bal2 = s.balance(addr2)
+    val bal3 = s.balance(addr3) + bal1 + bal2
 
-    val bal1 = s.balance(addr1, None)
-    log.info("before bal 1 " + bal1)
 
-    val bal2 = s.balance(addr2, None)
-    log.info("before bal 2 " + bal2)
+    val diff = blockchain.collectLposPortfolios {
+      case (addr1, p) if bal1 != 0L =>
+        Portfolio(p.balance, LeaseBalance(leaseInBalances.getOrElse(addr1, 0L) ,0), Map.empty)
 
-    val bal3 = s.balance(addr3, None)
-    log.info("before bal 3 " + bal3)
+      case (addr2, p) if bal2 !=  0L =>
+        Portfolio(p.balance, LeaseBalance(leaseInBalances.getOrElse(addr2, 0L) ,0), Map.empty)
 
-    val diff = s.collectLposPortfolios {
-      case (addr, p) if addr == addr1 && bal1 != 0L =>
-        Portfolio(-p.balance, LeaseBalance(0L, 0L), Map.empty)
-
-      case (addr, p) if addr == addr2 && bal2 != 0L =>
-        Portfolio(-p.balance, LeaseBalance(0L, 0L), Map.empty)
-
-      case (addr, p) if addr == addr3 =>
-        Portfolio(bal1 + bal2, p.lease, Map.empty)
+      case (addr3, p) if bal3 !=  0L =>
+        Portfolio(p.balance, LeaseBalance(leaseInBalances.getOrElse(addr3, 0L) ,0), Map.empty)
     }
 
     Diff.empty.copy(portfolios = diff)
