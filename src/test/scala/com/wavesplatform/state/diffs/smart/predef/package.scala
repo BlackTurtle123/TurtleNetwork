e5d93a343bcc0e46705543b2fe7f24287ca42e51
dserebryansky <denis.serebryansky.eu@gmail.com>
2019-01-10 18:52:10 +0300

    NODE-1270 Additional chainId renaming

--- src/test/scala/com/wavesplatform/state/diffs/smart/predef/package.scala
+++ src/test/scala/com/wavesplatform/state/diffs/smart/predef/package.scala
@@ -18,13 +18,13 @@ import shapeless.Coproduct
 package object predef {
   val chainId: Byte = 'u'
 
-  def runScript[T <: EVALUATED](script: String, version: Version, t: In, blockchain: Blockchain, networkByte: Byte): Either[String, T] = {
-    val Success(expr, _) = Parser.parseScript(script)
+  def runScript[T <: EVALUATED](script: String, version: ScriptVersion, t: In, blockchain: Blockchain, chainId: Byte): Either[String, T] = {
+    val Success(expr, _) = Parser(script)
     for {
       compileResult <- ExpressionCompilerV1(compilerContext(version), expr)
       (typedExpr, _) = compileResult
       evalContext = BlockchainContext.build(version,
-                                            networkByte,
+                                            chainId,
                                             Coeval.evalOnce(t),
                                             Coeval.evalOnce(blockchain.height),
                                             blockchain,
@@ -36,8 +36,8 @@ package object predef {
   def runScript[T <: EVALUATED](script: String, t: In = null): Either[String, T] =
     runScript[T](script, V1, t, EmptyBlockchain, chainId)
 
-  def runScript[T <: EVALUATED](script: String, t: In, networkByte: Byte): Either[String, T] =
-    runScript[T](script, V1, t, EmptyBlockchain, networkByte)
+  def runScript[T <: EVALUATED](script: String, t: In, chainId: Byte): Either[String, T] =
+    runScript[T](script, V1, t, EmptyBlockchain, chainId)
 
   def runScript[T <: EVALUATED](script: String, tx: Transaction, blockchain: Blockchain): Either[String, T] =
     runScript[T](script, V1, Coproduct(tx), blockchain, chainId)
